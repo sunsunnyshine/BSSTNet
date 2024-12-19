3210461d447b9c2984fbc12fb5424710c92c151d
3090Ti <1741834627@qq.com>
2024-12-19 15:24:27 +0800

    12.13: dist_train_worker add

---
+++ scripts/dist_train_worker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+GPUS=$1
+CONFIG=$2
+PORT=${PORT:-30033}
+
+# usage
+if [ $# -ne 2 ] ;then
+    echo "usage:"
+    echo "./scripts/dist_test.sh [number of gpu] [path to option file]"
+    exit
+fi
+
+PYTHONPATH="$(dirname $0)/..:${PYTHONPATH}" \
+python -m torch.distributed.launch --nproc_per_node=$GPUS --nnodes=2 --node_rank=1 --master_addr="115.156.212.64" --master_port=$PORT \
+    basicsr/train.py -opt $CONFIG --launcher pytorch
